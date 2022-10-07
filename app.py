f38d72bbec79bf689a4f5ea19d0a7c689355bcce
Shiv Srivastava <1983.shiv@gmail.com>
2022-10-07 08:53:43 +0530

    revised app with async and fastapi v3

--- app.py
+++ app.py
@@ -78,5 +78,5 @@ async def root():
 # app.run(app())
 
 
-if __name__ == '__main__':
-    uvicorn.run("app:app", port=8000, host='0.0.0.0')
+# if __name__ == '__main__':
+#     uvicorn.run("app:app", port=8000, host='0.0.0.0')
