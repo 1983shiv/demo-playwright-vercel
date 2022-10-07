be615400b417e746f9adbedd02c83610bd4301f7
Shiv Srivastava <1983.shiv@gmail.com>
2022-10-07 14:08:47 +0530

    revised requirements

--- working_cloud_scrap_async.py
+++ working_cloud_scrap_async.py
@@ -1,4 +1,4 @@
-# import asyncio
+import nest_asyncio
 from playwright.async_api import async_playwright
 # from flask import Flask
 import json
@@ -7,10 +7,10 @@ from bs4 import BeautifulSoup as bs
 from fastapi import FastAPI
 import uvicorn
 import subprocess
+from pyngrok import ngrok
 
 
 subprocess.run(["playwright", "install"])
-subprocess.run(["playwright", "install-deps"])
 
 
 async def scrap():
@@ -30,13 +30,10 @@ async def scrap():
         html = await page.content()
 
         soup = bs(html, 'html.parser')
-        # print(soup.prettify())
         allListing = soup.find_all(
             'article', attrs={'class': 'search-result-card'})
-        # print(len(allListing))
         for listing in allListing:
             if ("today" in listing.find_all("p")[3].text):
-                # print(listing.find_all("p")[3].text)
                 item = {
                     "title": listing.find_all("a")[0].text,
                     "url": listing.find_all("a")[1].text,
@@ -48,39 +45,19 @@ async def scrap():
                     "typeofjob": listing.find_all("p")[5].text
                 }
                 items.append(item)
-        # print(len(items))
         await browser.close()
     return items
 
 app = FastAPI()
 
-# asyncio.run(scrap())
-
 
 @app.get('/')
 async def root():
     items = await scrap()
-    # data = {"data": items, "Timestamp": time.time()}
-    # json_dump = await json.dumps(data)
-    # print("running server...")
     return items
 
+ngrok_tunnel = ngrok.connect(8000)
+print("Public URL : ", ngrok_tunnel.public_url)
+nest_asyncio.apply()
 
-# app = Flask(__name__)
-
-# @app.route('/')
-# async def home():
-#     # data = {"data": await scrap(), "Timestamp": time.time()}
-#     # json_dump = await json.dumps(data)
-#     # print("running server...")
-#     return scrap()
-
-# async def main():
-#     await scrap()
-
-
-# app.run(app())
-
-
-# if __name__ == '__main__':
-#     uvicorn.run("app:app", port=8000, host='127.0.0.1')
+uvicorn.run(app, port=8000)
